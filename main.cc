802c935cc5c7ab4779f9e7c9d931ab56dd8a02be
nskysd <nsky_sd@yahoo.com.tw>
2019-06-10 18:44:10 +0800

    2019061018

--- main.cc
+++ main.cc
@@ -1,21 +1,27 @@
 /* 
     Your main program goes here
 */
+
+
 #include <iostream>
 #include <vector>
 #include <stack>
 #include <string>
+#include <queue>
 
 #include "network_manager.h"
 #include "gplot.h"
 #include "path.h"
 
+
 using namespace std;
 
 
-int Eular_Path_Searching(NetworkManager* nm , vector<Vertex*> node_list);
+void Euler_Path_Searching(NetworkManager* nm , vector<Vertex*> node_list);
 int Accessible_Vertex_Identification(NetworkManager* nm , vector<Vertex*> node_list , string vertex);
 int Vertex_Number_Getting(vector<Vertex*> node_list , string vertex);
+void Graph_Eulerizing( NetworkManager* nm , vector<Vertex*> node_list , int Number_Of_Vertex , vector<Vertex*> Odd_Degree_Vertex , int Number_Of_Odd_Degree_Vertex );
+int Breadth_First_Search( NetworkManager* nm , vector<Vertex*> node_list , int Number_Of_Vertex , string Odd_Degree_Vertex );
 
 // create NetworkManager first
 NetworkManager *nm = new NetworkManager();
@@ -23,28 +29,19 @@ NetworkManager *nm = new NetworkManager();
 
 int main(int argc, char** argv){
 
-    /* start your program */
-
-
     //Read Graph
 //////////////////////////////////////////////////////////////////////////////////////////////////////
+    
     nm->interpret("Graph.txt");
     nm->print_all_e();
     nm->print_all_v();
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
 
 
     //Identify Odd Degree Vertex
 //////////////////////////////////////////////////////////////////////////////////////////////////////
+    
     Vertex* node = nm->get_all_nodes();
     vector<Vertex*> node_list;
 
@@ -75,57 +72,60 @@ int main(int argc, char** argv){
 
     
     for(int i = 0 ; i < Number_Of_Vertex ; i++)
-        cout<<"Degree["<<i<<"]="<<degree[i]<<endl;
+        //cout<<"Degree["<<i<<"]="<<degree[i]<<endl;
+        cout<<"Degree of Vertex \""<<node_list[i]->name<<"\" is "<<degree[i]<<"."<<endl;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////    
+    
+
+    //Eulerize Graph (If Graph is not Eulerian)
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    
+    vector<Vertex*> Odd_Degree_Vertex;
+    
+    int Number_Of_Odd_Degree_Vertex = 0;
+    for(int i = 0 ; i < Number_Of_Vertex ; i++)
+        if(degree[i]%2 != 0){
+            Odd_Degree_Vertex.push_back(node_list[i]) ;
+            Number_Of_Odd_Degree_Vertex++;
+        }
+    for(int i = 0 ; i < Number_Of_Odd_Degree_Vertex ; i++)
+        cout<<"Vertex \""<<Odd_Degree_Vertex[i]->name<<"\" is odd degree Vertex."<<endl;
+        
+    /*
+    if(Number_Of_Odd_Degree_Vertex = 2)
+        nm->connect(Odd_Degree_Vertex[0]->name,Odd_Degree_Vertex[1]->name);
+    //else if(Number_Of_Odd_Degree_Vertex > 2)
+        //Graph_Eulerizing( nm , Number_Of_Vertex , Odd_Degree_Vertex , Number_Of_Odd_Degree_Vertex );
+    */    
+        
+    Breadth_First_Search( nm , node_list , Number_Of_Vertex , Odd_Degree_Vertex[0]->name );    
+        
+    
 //////////////////////////////////////////////////////////////////////////////////////////////////////    
     
     
-    
-
-
-
-
-
-    
-    //Eular
+    //Search Euler Path
 //////////////////////////////////////////////////////////////////////////////////////////////////////    
-    nm->connect("b", "e");
+    //Euler_Path_Searching(nm , node_list);
 //////////////////////////////////////////////////////////////////////////////////////////////////////    
     
     
-
-
-
-
-
-
-
-    
-    Eular_Path_Searching(nm , node_list);
-    
-    
-    
-    
-
-    
-    
-
-    
-
     return 0;
+
 }
 
 
-
-int Eular_Path_Searching(NetworkManager* nm , vector<Vertex*> node_list){
+void Euler_Path_Searching( NetworkManager* nm , vector<Vertex*> node_list ) {
     
-    std::stack<std::string> Eular_Path;
-    std::stack<std::string> Temp_Eular_Path;
+    std::stack<std::string> Euler_Path ;
+    std::stack<std::string> Temp_Euler_Path ;
     
     nm->print_all_e();
     nm->print_all_v();
-    
-    
-    Temp_Eular_Path.push(node_list[0]->name);
+
+    Temp_Euler_Path.push(node_list[0]->name);
    
    
     /*
@@ -135,35 +135,29 @@ int Eular_Path_Searching(NetworkManager* nm , vector<Vertex*> node_list){
     /*
     cout << Accessible_Vertex_Identification( nm , node_list , node_list[0]->name ) << endl ;
     cout << Vertex_Number_Getting( node_list , "a" ) << endl ;
-    */
-    
-    
-    
-    
-    
-    
+    */  
     
     int Number_Of_Vertex;
     int Start_Vertex_Number;
     Number_Of_Vertex=node_list.size();
-    while(!Temp_Eular_Path.empty()) {
+    while(!Temp_Euler_Path.empty()) {
         
-        Start_Vertex_Number = Vertex_Number_Getting( node_list , Temp_Eular_Path.top() );
+        Start_Vertex_Number = Vertex_Number_Getting( node_list , Temp_Euler_Path.top() );
         
         if( Accessible_Vertex_Identification( nm , node_list , node_list[Start_Vertex_Number]->name ) ) {
             
-            Eular_Path.push(Temp_Eular_Path.top());
-            Temp_Eular_Path.pop();
+            Euler_Path.push(Temp_Euler_Path.top());
+            Temp_Euler_Path.pop();
         
         }
-        else {
-            for( int i = 0 ; i < Number_Of_Vertex ; i++ ) {
+        else
+            for( int i = 0 ; i < Number_Of_Vertex ; i++ )
                 if( ! ( nm->connected_d( node_list[Start_Vertex_Number]->name , node_list[i]->name ) ) ) {
                   
                     nm->disconnect( node_list[Start_Vertex_Number]->name , node_list[i]->name );
                     cout << "Vertex \""<<node_list[Start_Vertex_Number]->name<<"\" and Vertex \" "<<node_list[i]->name<<"\" is disconnected"<<endl;
                     nm->print_all_e();
-                    Temp_Eular_Path.push( node_list[i]->name ) ;
+                    Temp_Euler_Path.push( node_list[i]->name ) ;
                     break;
                 }    
 
@@ -172,38 +166,33 @@ int Eular_Path_Searching(NetworkManager* nm , vector<Vertex*> node_list){
                     nm->disconnect( node_list[i]->name , node_list[Start_Vertex_Number]->name );
                     cout << "Vertex \""<<node_list[Start_Vertex_Number]->name<<"\" and Vertex \" "<<node_list[i]->name<<"\" is disconnected"<<endl;
                     nm->print_all_e();
-                    Temp_Eular_Path.push( node_list[i]->name ) ;
+                    Temp_Euler_Path.push( node_list[i]->name ) ;
                     break;
                 }
-                
-            }
-                
-        }    
-
     }
     
     
     
-        cout << Eular_Path.top()<<endl;
-        Eular_Path.pop();
-        cout << Eular_Path.top()<<endl;
-        Eular_Path.pop();
-        cout << Eular_Path.top()<<endl;
-        Eular_Path.pop();
-        cout << Eular_Path.top()<<endl;
-        Eular_Path.pop();
-        cout << Eular_Path.top()<<endl;
-        Eular_Path.pop();
-        cout << Eular_Path.top()<<endl;
-        Eular_Path.pop();
-        cout << Eular_Path.top()<<endl;
-        Eular_Path.pop();
-        cout << Eular_Path.top()<<endl;
-        Eular_Path.pop();
-        cout << Eular_Path.top()<<endl;
-        Eular_Path.pop();
-    
-    return 0;
+        cout << Euler_Path.top()<<endl;
+        Euler_Path.pop();
+        cout << Euler_Path.top()<<endl;
+        Euler_Path.pop();
+        cout << Euler_Path.top()<<endl;
+        Euler_Path.pop();
+        cout << Euler_Path.top()<<endl;
+        Euler_Path.pop();
+        cout << Euler_Path.top()<<endl;
+        Euler_Path.pop();
+        cout << Euler_Path.top()<<endl;
+        Euler_Path.pop();
+        cout << Euler_Path.top()<<endl;
+        Euler_Path.pop();
+        cout << Euler_Path.top()<<endl;
+        Euler_Path.pop();
+        cout << Euler_Path.top()<<endl;
+        Euler_Path.pop();
+        cout << Euler_Path.top()<<endl;
+        Euler_Path.pop();
     
 }
 
@@ -240,4 +229,196 @@ int Vertex_Number_Getting(vector<Vertex*> node_list , string vertex){
 
     return Vertex_Number;
 }
+
+
+/*
+void Graph_Eulerizing( NetworkManager* nm , vector<Vertex*> node_list , int Number_Of_Vertex , vector<Vertex*> Odd_Degree_Vertex , int Number_Of_Odd_Degree_Vertex ){
+    
+    std::queue<std::string> Breadth_First_Search_Path ;
+    
+    int distance [Number_Of_Vertex] ;
+    int color[Number_Of_Vertex] ; //0 is white, 1 is gray, and 2 is black.
+    for(int i = 0 ; i < Number_Of_Vertex ; i++) {
+    
+        color[i] = 0 ;
+        distance [i] = 9999 ;
+    
+    }
+    
+    string parent[Number_Of_Vertex] ;
+    
+    Breadth_First_Search_Path.push(Odd_Degree_Vertex[0]);
+    
+    while( ! Breadth_First_Search_Path.empty() ) {
+    
+        for(int i = 0 ; i < Number_Of_Vertex ; i++ ) {
+            
+            if( ! ( nm->connected( Breadth_First_Search_Path.front() , node_list[i]->name ) ) ) {
+                
+                color[i] = 1 ;
+                distance [i] = distance [0] + 1 ;
+                parent[i] = Breadth_First_Search_Path.front() ;
+                Breadth_First_Search_Path.push(node_list[i]->name) ;
+                
+            }
+        }  
+        
+        color[i] = 2 ;
+    
+    }
+    
+    if( Number_Of_Odd_Degree_Vertex = 2 )
+        if( ! ( nm->connected( Odd_Degree_Vertex[Number_Of_Odd_Degree_Vertex-2]->name , Odd_Degree_Vertex[Number_Of_Odd_Degree_Vertex-1]->name ) ) )
+            nm -> connect( Odd_Degree_Vertex[Number_Of_Odd_Degree_Vertex-2]->name , Odd_Degree_Vertex[Number_Of_Odd_Degree_Vertex-1]->name ) ;
+        else if()
+    else
+
+}
+*/
+
+int Breadth_First_Search( NetworkManager* nm , vector<Vertex*> node_list , int Number_Of_Vertex , string Odd_Degree_Vertex ) {
+
+    std::queue<std::string> Breadth_First_Search_Path ;
+    
+    int distance [Number_Of_Vertex] ;
+    int color[Number_Of_Vertex] ; //0 is white, 1 is gray, and 2 is black.
+    for(int i = 0 ; i < Number_Of_Vertex ; i++) {
+    
+        color[i] = 0 ;
+        distance [i] = 9999 ;
+    
+    }
+    
+    
+    
+    
+    
+    for(int i = 0 ; i < Number_Of_Vertex ; i++ ){
+        cout<<i<<endl;
+        cout<<"color is "<<color[i]<<endl;
+    }
+    
+    
+    string parent[Number_Of_Vertex] ;
+    string aaa ;
+    
+    distance [1] = 0;
+    Breadth_First_Search_Path.push(Odd_Degree_Vertex);
+    
+    //cout<<Breadth_First_Search_Path.front()<<endl;
+    //cout<<node_list[0]->name<<endl;
+    
+    while( ! Breadth_First_Search_Path.empty() ) {
+    
+        aaa=Breadth_First_Search_Path.front();
+        Breadth_First_Search_Path.pop();
+        cout<<aaa<<endl;
+        
+        for(int i = 0 ; i < Number_Of_Vertex ; i++ ) {
+            
+            
+            //cout<<i<<endl;
+            cout<<nm->connected( aaa , node_list[i]->name )<<endl;
+            cout<<color[i]<<endl;
+            
+            if( ( ! ( nm->connected( aaa , node_list[i]->name ) ) ) && ( color[i] == 0 ) ) {
+                cout<<i<<" yes"<<endl;
+                color[i] = 1 ;
+                distance [i] = distance [1] + 1 ;
+                cout<<"distance is "<<distance [i]<<endl;
+                parent[i] = Breadth_First_Search_Path.front() ;
+                Breadth_First_Search_Path.push(node_list[i]->name) ;
+                //cout<<node_list[i]->name<<endl;
+            }
+            else{
+                cout<<i<<endl;
+                cout<<"no"<<endl;
+            }
+        }  
+        
+        /*
+        
+        if(aaa=="b")
+            color[1] = 2 ;
+        
+        
+        switch (aaa) { 
+            case 'a': 
+                color[0] = 2 ; 
+                break; 
+            case 'b': 
+                color[1] = 2 ; 
+                break; 
+            case 'c': 
+                color[2] = 2 ; 
+                break; 
+            case 'd': 
+                color[3] = 2 ; 
+                break;
+            case 'e': 
+                color[4] = 2 ; 
+                break; 
+            case 'f': 
+                color[5] = 2 ; 
+                break; 
+            case 'g': 
+                color[6] = 2 ; 
+                break; 
+            //default: 
+                //; 
+        }
+        
+        
+        
+        */
+        
+        
+        
+        
+        /*
+        cout<<Breadth_First_Search_Path.front()<<endl;
+        Breadth_First_Search_Path.pop();
+        cout<<Breadth_First_Search_Path.front()<<endl;
+        Breadth_First_Search_Path.pop();
+        cout<<Breadth_First_Search_Path.front()<<endl;
+        Breadth_First_Search_Path.pop();
+        cout<<Breadth_First_Search_Path.front()<<endl;
+        Breadth_First_Search_Path.pop();
+        cout<<Breadth_First_Search_Path.front()<<endl;
+        Breadth_First_Search_Path.pop();
+        cout<<Breadth_First_Search_Path.front()<<endl;
+        Breadth_First_Search_Path.pop();
+        */
+   
+    }
+    
+    /*
+    for(int i = 0 ; i < Number_Of_Vertex ; i++ ){
+        cout<<i<<endl;
+        cout<<distance[i]<<endl;
+    }
+    */
+
+    return 0 ;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
